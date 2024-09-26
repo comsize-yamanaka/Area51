5fbf77029169a9b93ed753a9d4ec9b134c82d153
comsize-yamanaka <yuta.yamanaka@comsize.com>
2024-09-26 11:57:37 +0900

    タスク登録処理の作成

--- src/main/java/servlet/TaskRegisterServlet.java
+++ src/main/java/servlet/TaskRegisterServlet.java
@@ -2,6 +2,7 @@ package servlet;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,8 +15,12 @@ import javax.servlet.http.HttpServletResponse;
 
 import model.dao.CategoryDAO;
 import model.dao.StatusDAO;
+import model.dao.TaskListDAO;
+import model.dao.UserDAO;
 import model.entity.CategoryBean;
 import model.entity.StatusBean;
+import model.entity.TaskBean;
+import model.entity.UserBean;
 
 /**
  * Servlet implementation class TaskRegisterServlet
@@ -31,17 +36,21 @@ request.setCharacterEncoding("UTF-8");
 		
 		List<CategoryBean> categoryList = new ArrayList<>();
 		List<StatusBean> statusList = new ArrayList<>();
+		List<UserBean> userList = new ArrayList<>();
 		
 		try {
 			
 			CategoryDAO cateDAO = new CategoryDAO();
 			StatusDAO statDAO = new StatusDAO();
+			UserDAO userDAO = new UserDAO();
 			
 			categoryList = cateDAO.selectAll();
 			statusList = statDAO.selectAll();
+			userList = userDAO.selectAll();
 			
 			request.setAttribute("categoryList", categoryList);
 			request.setAttribute("statusList", statusList);
+			request.setAttribute("userList", userList);
 			
 		}catch(SQLException | ClassNotFoundException e) {
 			e.printStackTrace();
@@ -55,7 +64,48 @@ request.setCharacterEncoding("UTF-8");
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
 		
+		String taskName = (String)request.getParameter("taskName");
+		int categoryId = Integer.parseInt(request.getParameter("categoryId"));
+		LocalDate limitDate = LocalDate.parse(request.getParameter("limitDate"));
+		String userId = request.getParameter("userId");
+		String statusCode = request.getParameter("statusCode");
+		String memo = request.getParameter("memo");
+		
+		int count = 0;
+		String Path = "task-register-failure.jsp";
+		
+		TaskBean taskBean = new TaskBean();
+		
+		taskBean.setTaskName(taskName);
+		taskBean.setCategoryId(categoryId);
+		taskBean.setLimitDate(limitDate);
+		taskBean.setUserId(userId);
+		taskBean.setStatusCode(statusCode);
+		taskBean.setMemo(memo);
+		
+		try {
+			TaskListDAO dao = new TaskListDAO();
+			
+			count = dao.taskRegister(taskBean);
+			
+			//登録成功ならタスク登録完了画面へ遷移
+			if(count != 0) {
+				Path = "task-register-success";
+				
+				request.setAttribute("taskBean", taskBean);
+				
+			}else {
+				//登録失敗処理
+			}
+			
+		}catch(SQLException | ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		RequestDispatcher rd = request.getRequestDispatcher(Path);
+		rd.forward(request, response);
 	}
 
 }
