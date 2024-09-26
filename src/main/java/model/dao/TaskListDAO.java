abfe204d4c5bac4571760258a9dbe32522b93fdb
comsize-yamanaka <yuta.yamanaka@comsize.com>
2024-09-26 15:15:04 +0900

    タスク登録処理の追加・修正

--- src/main/java/model/dao/TaskListDAO.java
+++ src/main/java/model/dao/TaskListDAO.java
@@ -5,10 +5,12 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import model.entity.TaskBean;
 import model.entity.TaskListBean;
 
 /**
@@ -20,7 +22,7 @@ import model.entity.TaskListBean;
  * 
  */
 public class TaskListDAO {
-	
+
 	/**
 	 *タスク一覧を返します
 	 * @return タスク一覧　taskList
@@ -31,9 +33,8 @@ public class TaskListDAO {
 			throws SQLException, ClassNotFoundException {
 
 		List<TaskListBean> taskList = new ArrayList<TaskListBean>();
-		String sql = "SELECT t1.task_name, t2.category_name, t1.limit_date, t3.user_name, t4.status_name, t1.memo FROM t_task t1 inner join m_category t2 on t1.category_id = t2.category_id inner join m_user t3 on t1.user_id = t3.user_id inner join m_status t4 on t1.status_code = t4.status_code;";	
-		
-		
+		String sql = "SELECT t1.task_name, t2.category_name, t1.limit_date, t3.user_name, t4.status_name, t1.memo FROM t_task t1 inner join m_category t2 on t1.category_id = t2.category_id inner join m_user t3 on t1.user_id = t3.user_id inner join m_status t4 on t1.status_code = t4.status_code;";
+
 		try (Connection con = ConnectionManager.getConnection();
 				Statement stmt = con.createStatement();
 				ResultSet res = stmt.executeQuery(sql)) {
@@ -46,7 +47,7 @@ public class TaskListDAO {
 				String userName = res.getString("user_name");
 				String statusName = res.getString("status_name");
 				String memo = res.getString("memo");//????
-				
+
 				TaskListBean task = new TaskListBean();
 				task.setTaskName(taskName);//
 				task.setCategoryName(categoryName);
@@ -60,9 +61,7 @@ public class TaskListDAO {
 		}
 		return taskList;
 	}
-	
-	
-	
+
 	/**
 	 * 指定されたタスクを処理します
 	 * @param taskId
@@ -82,8 +81,7 @@ public class TaskListDAO {
 		}
 		return processingNumber;
 	}
-	
-	
+
 	/**
 	 * 指定されたタスクIDのタスクを1件返します
 	 * @param taskId
@@ -94,7 +92,7 @@ public class TaskListDAO {
 	public TaskListBean selectTask(int taskId) throws SQLException, ClassNotFoundException {
 
 		String sql = "SELECT t1.task_name, t2.category_name, t1.limit_date, t3.user_name, t4.status_name, t1.memo FROM t_task t1 inner join m_category t2 on t1.category_id = t2.category_id inner join m_user t3 on t1.user_id = t3.user_id inner join m_status t4 on t1.status_code = t4.status_code where task_id=?;";
-		
+
 		TaskListBean taskDetail = new TaskListBean();
 		try (Connection con = ConnectionManager.getConnection();
 				PreparedStatement pstmt = con.prepareStatement(sql);) {
@@ -116,8 +114,33 @@ public class TaskListDAO {
 		return taskDetail;
 	}
 	
-	
-	
-	
+	/**
+	 * タスク登録処理
+	 * @param taskBean
+	 * @return
+	 * @throws SQLException
+	 * @throws ClassNotFoundException
+	 */
+	public int taskRegister(TaskBean taskBean) throws SQLException, ClassNotFoundException {
+		int count = 0;
+		String sql = "INSERT INTO task_db.t_task(task_name, category_id, limit_date, user_id, status_code, memo, create_datetime, update_datetime) "
+				+ "VALUES(?,?,?,?,?,?,?,?);";
 
+		try (Connection con = ConnectionManager.getConnection()) {
+			PreparedStatement pstmt = con.prepareStatement(sql);
+
+			pstmt.setString(1, taskBean.getTaskName());
+			pstmt.setInt(2, taskBean.getCategoryId());
+			pstmt.setDate(3, java.sql.Date.valueOf(taskBean.getLimitDate()));
+			pstmt.setString(4, taskBean.getUserId());
+			pstmt.setString(5, taskBean.getStatusCode());
+			pstmt.setString(6, taskBean.getMemo());
+			pstmt.setTimestamp(7, Timestamp.valueOf(LocalDateTime.now()));
+			pstmt.setTimestamp(8, Timestamp.valueOf(LocalDateTime.now()));
+
+			count = pstmt.executeUpdate();
+		}
+
+		return count;
+	}
 }
