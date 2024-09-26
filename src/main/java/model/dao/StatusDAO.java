741e7ca678c212564dde0b8643bbcb13cd1124d4
comsize-yamanaka <yuta.yamanaka@comsize.com>
2024-09-26 15:03:27 +0900

    ステータスコードを指定して、ステータス名を取得する処理を追加

--- src/main/java/model/dao/StatusDAO.java
+++ src/main/java/model/dao/StatusDAO.java
@@ -1,6 +1,7 @@
 package model.dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -12,8 +13,18 @@ import model.entity.StatusBean;
 /**
  * @author Yuta Yamanaka
  */
+
+/**
+ * m_statusテーブルのDAO
+ */
 public class StatusDAO {
 	
+	/**
+	 * m_statusテーブルのレコードを全て取得
+	 * @return
+	 * @throws SQLException
+	 * @throws ClassNotFoundException
+	 */
 	public List<StatusBean> selectAll() throws SQLException, ClassNotFoundException{
 		List<StatusBean> statusList = new ArrayList<>();
 		
@@ -31,5 +42,35 @@ public class StatusDAO {
 			}
 		}
 		return statusList;
+	}
+	
+	/**
+	 * ステータスコードを指定して、ステータス名を取得
+	 * @param statusCode
+	 * @return
+	 * @throws SQLException
+	 * @throws ClassNotFoundException
+	 */
+	public String selectStatusName(String statusCode) throws SQLException, ClassNotFoundException{
+		String statusName = "";
+		String sql = "SELECT * FROM m_status WHERE status_code = ?";
+		
+		try(Connection con = ConnectionManager.getConnection()) {
+			PreparedStatement pstmt = con.prepareStatement(sql);
+			
+			pstmt.setString(1, statusCode);
+			
+			ResultSet res = pstmt.executeQuery();
+			
+			//ステータスコードに対応するステータス名がないとき
+			if(res.equals(null)) {
+				
+			}else {
+				res.next();
+				statusName = res.getString("status_name");
+			}
+		}
+		
+		return statusName;
 	}
 }
